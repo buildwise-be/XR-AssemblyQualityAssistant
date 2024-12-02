0ad10aa5b0f1895fd24788a9c0e030b586231359
Arnaud Jopart <arnaudjopart@users.noreply.github.com>
2024-12-02 16:41:40 +0100

    Input field in pending message

--- Assets/_project/Scripts/Views/PendingComment.cs
+++ Assets/_project/Scripts/Views/PendingComment.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 internal class PendingComment : MonoBehaviour
 {
     [SerializeField] private TMP_Text _text;
     private int _index;
-    
+    [SerializeField] private TMP_InputField _inputField;
+    [SerializeField] private RectTransform _textField;
+    private RectTransform _inputFieldRectTransform;
+    [SerializeField] private float _offset=10;
     public Action<int> OnDeleteButtonEventClick { get; set; }
+
+
+    private void Awake()
+    {
+        _inputFieldRectTransform = _inputField.GetComponent<RectTransform>();
+    }
 
     public void SetText(string arg0)
     {
-        _text.SetText(arg0);
+        _inputField.text = arg0;
     }
 
     public void SetIndex(int index)
@@ -22,5 +32,12 @@ internal class PendingComment : MonoBehaviour
     public void OnDeleteButtonClick()
     {
         OnDeleteButtonEventClick?.Invoke(_index);
+    }
+
+    private void Update()
+    {
+        var sizeDelta = _inputFieldRectTransform.sizeDelta;
+        sizeDelta.y = _textField.sizeDelta.y+_offset;
+        _inputFieldRectTransform.sizeDelta = sizeDelta;
     }
 }
